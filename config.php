062dec3cb83eb1afdf6aa3055ae2c18c0c29b68d
EugeneBruev <34741766+swatteam1@users.noreply.github.com>
2021-04-30 13:30:49 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
     require('parse.php');
-	//remove this comment and encode before deploy
+	//remove this comment and encode after deploy
   //  'ZGJfc2VydmVyOjEyNy4wLjAuMTsNCiAgICBkYl91c2VyOmFkbWluOw0KICAgIGRiX3Bhc3N3b3JkOk1lZ2FQYXNzd29yZDEhOw0KICAgIGRiX2RiOnBvcnRhbA=='
   $str1=base64_encode($string_config);
    
